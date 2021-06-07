485feb79d820b20a47466e561dda8d65e1d97f1e
Atul Singh <asatulsingh20@gmail.com>
2021-06-07 14:52:56 +0530

    Fixes

--- mailer.js
+++ mailer.js
@@ -1,35 +1,45 @@
 const express = require("express");
 const mailer = require("nodemailer");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 const QRcode = require("easyqrcodejs-nodejs");
+const { google } = require("googleapis");
 require("dotenv/config");
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(json());
+
+const OAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+OAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 app.get("/", (req, res) => {
   res.send("Mailer for my applications");
 });
 
-let transporter = mailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: process.env.USER,
-    pass: process.env.PASS,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-
 app.post("/withQRcode", async (req, res) => {
   try {
+    let accessToken = await OAuth2Client.getAccessToken();
+    let transporter = mailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.USER,
+        // pass: process.env.PASS,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
     const API_KEY = req.body.API_KEY;
     if (API_KEY === process.env.API_KEY) {
       let QR = new QRcode({
@@ -45,9 +55,9 @@ app.post("/withQRcode", async (req, res) => {
       let mailOption = {
         from: process.env.USER,
         to: req.body.email,
-        subject: "Welcome!",
-        text: "Your QR code is attached with the email",
-        html: '<img src="cid:outQR"/>',
+        subject: "Table Details!",
+        text: "Your Table is Reserved. Just show the below QR Code 😀",
+        html: '<p>Your Table is Reserved. Just show the below QR Code 😀</p><img src="cid:outQR"/>',
         attachments: [{ filename: "out.png", path: "./out.png", cid: "outQR" }],
       };
 
